fec9c16e5320c1c0485db83f6c7b95799167ecb7
kobebryant60308 <kobebryant60308@gmail.com>
2021-05-04 23:43:11 +0800

    first commit

---
+++ Desktop/react_hw/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Signin from './sign';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        div > { Signin() } < /div> <
+        /div>
+    );
+}
+
+export default App;
